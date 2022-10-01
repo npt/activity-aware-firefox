45c07c43f55b6a7baeb8ac118033a4ce09ea644d
Matija Šuklje <matija@suklje.name>
2022-10-01 21:24:08 +0200

    Made the repository REUSE-compliant again

--- template_example/user.js
+++ template_example/user.js
@@ -1,2 +1,4 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+// SPDX-License-Identifier: CC0-1.0
+// SPDX-FileCopyrightText: © 2021 Matija Šuklje <matija@suklje.name>
 
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
